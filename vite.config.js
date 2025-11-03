097b94264f7d644d3e1a21db75d4e31bb9a5e4e5
GracaGikanga <gracagikanga@gmail.com>
2025-11-04 01:30:27 +0300

    Fixed the base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-js-jsx-and-css-mastering-front-end-development-GracaGikanga/',
   plugins: [
     react({
       babel: {
