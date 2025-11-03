7419fb90425c5445e243569852579e6b03727a54
GracaGikanga <gracagikanga@gmail.com>
2025-11-04 01:42:27 +0300

    updated base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: '/react-js-jsx-and-css-mastering-front-end-development-GracaGikanga/',
   plugins: [
